b2fa34ad6261dae8906817eaf6c5ff28d965160b
esmyth01 <evsmyth@yahoo.com>
2016-02-25 18:33:11 -0800

    Worked on sidebar added some images

--- front-page.php
+++ front-page.php
@@ -23,14 +23,54 @@
 
       <div class="homepage-sidebar">
 
-        <div class="homepage-projects">
+        <div class="sidebar-project-categories">
 
-              <div class="homepage-projects-h3"><h3>Projects</h3></div>
+              <div class="sidebar-h3"><h3>Project Categories</h3></div>
 
               <ul>
                 <?php wp_list_categories( 'title_li' ); ?>
               </ul>
             </div>
+
+            <div class="homepage-widget-item">
+      <div class="sidebar-h3"><h3>Latest Projects</h3></div>
+      <ul>
+
+        <?php rewind_posts(); ?>
+        <?php query_posts('showposts=2'); ?>
+        <?php while (have_posts()) : the_post(); ?>
+          <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+
+          <?php the_post_thumbnail('medium'); ?>
+
+          <?php echo get_excerpt(); ?>
+
+        <?php endwhile; ?>
+      </ul>
+
+
+    </div>
+
+    <div class="homepage-photography">
+
+      <div class=sidebar-h3><h3><a href="<?php echo get_permalink( get_page_by_path( 'photography' ) ) ?>">Photography</a></h3></div>
+
+
+      <?php $query = new WP_Query( array( "name" => "photography", "post_type" => "page" ));  ?>
+
+      <?php if ($query->have_posts()) : while($query->have_posts()) : $query->the_post(); //start loop ?>
+
+        <div class="homepage-photography-image"><?php the_post_thumbnail( 'medium' ); ?></div>
+        <div class="homepage-photography-p"><?php echo get_excerpt(); ?></div>
+
+
+
+      <?php endwhile; endif;  //end loop?>
+      <?php wp_reset_postdata() ?>
+
+
+    </div>
+
 
       </div><!--end homepage-sidebar-->
 
