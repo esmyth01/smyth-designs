3d25764615489653bfacbdcb52fad71cb27e5682
esmyth01 <evsmyth@yahoo.com>
2016-03-29 18:17:52 -0700

    Added navigation and other stuff to footer

--- footer.php
+++ footer.php
@@ -2,6 +2,44 @@
 </div><!--end wrapper-->
 
 <footer>
+  <div class="footer-content">
+
+  <div class="footer-inner">
+
+    <h4>SITEMAP</h4>
+
+
+<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+  </div>
+
+  <div class="footer-inner">
+
+    <h4>RESOURCES</h4>
+    <ul>
+      <li><a href="http://www.github.com">Github</a></li>
+      <li><a href="http://www.stackoverflow.com">Stack Overflow</a></li>
+      <li><a href="http://www.w3schools.com">W3 Schools</a></li>
+      <li><a href="http://www.smashingmagazine.com">Smashing Magazine</a></li>
+
+    </ul>
+
+  </div>
+
+  <div class="footer-inner">
+<h4>FRIENDS</h4>
+
+
+    <ul>
+      <li><a href="http://www.premiumdw.com">PremiumDW</a></li>
+      <li><a href="https://thomaswilburn.net">Thomas Wilburn</a></li>
+      
+
+    </ul>
+
+
+  </div>
+
+</div>
 
 </footer>
 
--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 <div class="nav-box">
 <div class="jquery-nav">
 				<h4 class="jquery-title"><a href="#">Menu<div class="menu-icon"><img src="<?php bloginfo('template_url'); ?>/images/menu-icon-large.png"></div></a></h4>
-				<?php wp_nav_menu(); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 			</div>
 
 </div>
